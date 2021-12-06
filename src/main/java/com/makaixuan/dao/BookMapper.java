c7e668b7a207ec0124c43c526dc97b476c25b413
makaixuan <lanqiuxiaotong@outlook.com>
2021-12-06 14:36:23 +0800

    update BookMapper comment upd

--- src/main/java/com/makaixuan/dao/BookMapper.java
+++ src/main/java/com/makaixuan/dao/BookMapper.java
@@ -6,18 +6,33 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface BookMapper {
+    /***
+    * insert books
+    */
     // 增加
     int addBook(Books books);
 
+    /***
+    * delete book
+    */
     // 删除
     int delBookById(@Param("bookId") int id);
 
+    /***
+    * modify books
+    */
     // 修改
     int updBook(Books books);
 
+    /***
+    * select book
+    */
     // 查看一个
     Books selBook(@Param("bookId") int id);
 
+    /***
+    * select books
+    */
     // 查看所有
     List<Books> selBooks();
 }
